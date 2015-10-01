1f6e95f019eb54113bcc233b19304674052c5515
Rick Wight <fmwight@wpi.edu>
2015-10-01 12:39:25 -0400

    Added FootballGameRunner class to play games and print out results to the command line

---
+++ src/FootballSim/FootballGameRunner.java
@@ -0,0 +1,15 @@
+package FootballSim;
+
+/**
+ * 
+ * @author Rick
+ * Plays football games and outputs results to the command line
+ */
+public class FootballGameRunner {
+	static FootballGame game = new FootballGame();
+
+	public static void main(String[] args) {
+		game.fbField.printField();
+	}
+
+}
