4abea5d2cc22eef7f7fbdb9742458394e79ec528
Rick Wight <fmwight@wpi.edu>
2015-10-05 00:15:01 -0400

    implemented test of Field constructor.

--- test/FootballSim/FieldTest.java
+++ test/FootballSim/FieldTest.java
@@ -2,13 +2,19 @@ package FootballSim;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FieldTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Field field1;
+	
+	@Before
+	public void setUpField() {
+		field1 = new Field();
 	}
 
+	@Test
+	public void constructorTest() {
+		assertNotNull(field1);
+	}
 }
