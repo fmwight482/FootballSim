1a4e2339be59e8fcb0e0f4b401e7748d8e732093
Rick Wight <fmwight@wpi.edu>
2015-10-05 16:27:13 -0400

    Added some tests for FootballGame class

--- test/FootballSim/FootballGameTest.java
+++ test/FootballSim/FootballGameTest.java
@@ -2,13 +2,28 @@ package FootballSim;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FootballGameTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	FootballGame aGame;
+	
+	@Before
+	public void setup() {
+		aGame = new FootballGame();
 	}
-
+	
+	@Test
+	public void constructorTest() {
+		assertNotNull(aGame);
+		assertNotNull(aGame.fbField);
+		assertNotNull(aGame.fbTimer);
+		assertNotNull(aGame.QB);
+		assertNotNull(aGame.WR);
+	}
+	
+	@Test
+	public void runRouteTest() {
+		fail("not yet implemented");
+	}
 }
