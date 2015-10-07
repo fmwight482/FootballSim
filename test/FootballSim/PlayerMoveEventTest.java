96a39d4eeab1ececd253fae90724cec74a5f527b
Rick Wight <fmwight@wpi.edu>
2015-10-07 15:13:38 -0400

    Started adding tests for PlayerMoveEvent

--- test/FootballSim/PlayerMoveEventTest.java
+++ test/FootballSim/PlayerMoveEventTest.java
@@ -2,10 +2,22 @@ package FootballSim;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerMoveEventTest {
-
+	PlayerMoveEvent playerMove1;
+	PlayerMoveEvent playerMove2;
+	FieldCoordinate coord1;
+	FieldCoordinate coord2;
+	FieldCoordinate coord3;
+	FieldCoordinate coord4;
+	
+	@Before
+	public void setup() {
+		//playerMove1 = new PlayerMoveEvent();
+	}
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
