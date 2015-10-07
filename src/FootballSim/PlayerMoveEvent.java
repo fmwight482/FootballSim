87f2d89c46e7e968aba12c7fe68c313fd13d5f94
Rick Wight <fmwight@wpi.edu>
2015-10-07 11:36:28 -0400

    Added copy constructor getters and setters for oldCoord, newCoord, and player variables in PlayerMoveEvent.

--- src/FootballSim/PlayerMoveEvent.java
+++ src/FootballSim/PlayerMoveEvent.java
@@ -6,19 +6,47 @@ package FootballSim;
  *
  */
 public class PlayerMoveEvent extends absFootballEvent implements IFootballEvent {
-	FieldCoordinate oldCoord;
-	FieldCoordinate newCoord;
-	FootballPlayer player;
+	private FieldCoordinate oldCoord;
+	private FieldCoordinate newCoord;
+	private FootballPlayer player;
 	
 	public PlayerMoveEvent(int aTime, FieldCoordinate anOldCoord, FieldCoordinate aNewCoord, FootballPlayer aPlayer) {
 		super(aTime);
-		oldCoord = anOldCoord;
-		newCoord = aNewCoord;
-		player = aPlayer;
+		setOldCoord(anOldCoord);
+		setNewCoord(aNewCoord);
+		setPlayer(aPlayer);
 	}
 
 	@Override
 	public void executeEvent() {
 		// TODO move player from oldCoord to newCoord
+	}
+	
+	public FieldCoordinate getOldCoord() {
+		return oldCoord;
+	}
+
+	public void setOldCoord(FieldCoordinate anOldCoord) {
+		oldCoord = new FieldCoordinate(anOldCoord);
+	}
+
+	public FieldCoordinate getNewCoord() {
+		return newCoord;
+	}
+
+	public void setNewCoord(FieldCoordinate aNewCoord) {
+		newCoord = new FieldCoordinate(aNewCoord);
+	}
+
+	public FootballPlayer getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(FootballPlayer aPlayer) {
+		player = new FootballPlayer(aPlayer);
+	}
+
+	public boolean equals(Object o) {
+		return false;
 	}
 }
