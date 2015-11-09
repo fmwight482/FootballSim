69f822f0a09954479e2671b3710b35503faded37
Rick Wight <fmwight@wpi.edu>
2015-11-09 11:21:33 -0500

    Created and tested ComparableCoordinate class to hold sortable field coordinates

---
+++ src/FootballSim/ComparableCoordinate.java
@@ -0,0 +1,47 @@
+package FootballSim;
+
+/**
+ * Class to hold vector coordinates representing individual steps in larger player movements 
+ * and sort them by time indexes. Used as an intermediate step when creating PlayerMoveEvents. 
+ * @author Rick
+ *
+ */
+public class ComparableCoordinate extends FieldCoordinate implements Comparable<ComparableCoordinate> {
+	/**
+	 * The time the player would move to this coordinate. Value is accurate relative to other 
+	 * steps but does not necessarily correspond to a "real" game time.
+	 */
+	int time;
+	
+	/**
+	 * Standard constructor
+	 * @param vert
+	 * @param horz
+	 * @param aTime
+	 */
+	public ComparableCoordinate(int vert, int horz, int aTime) {
+		super(vert, horz);
+		time = aTime;
+	}
+	
+	public ComparableCoordinate(ComparableCoordinate otherCoordinate) {
+		super(otherCoordinate.getVert(), otherCoordinate.getHorz());
+		time = otherCoordinate.getTime();
+	}
+	
+	public int getTime() {
+		return time;
+	}
+
+	@Override
+	public int compareTo(ComparableCoordinate otherCoord) {
+		int comparison = 0;
+		if (time < otherCoord.getTime()) {
+			comparison = -1;
+		}
+		else if(time > otherCoord.getTime()) {
+			comparison = 1;
+		}
+		return comparison;
+	}
+}
---
+++ test/FootballSim/ComparableCoordinateTest.java
@@ -0,0 +1,40 @@
+package FootballSim;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ComparableCoordinateTest {
+	ComparableCoordinate coord1;
+	ComparableCoordinate coord2;
+	ComparableCoordinate coord3;
+	ComparableCoordinate coord4;
+	
+	@Before
+	public void setup() {
+		coord1 = new ComparableCoordinate(1, 0, 0);
+		coord2 = new ComparableCoordinate(0, 1, 50);
+		coord3 = new ComparableCoordinate(1, 1, 100);
+		coord4 = new ComparableCoordinate(coord1);
+	}
+
+	@Test
+	public void constructorTest() {
+		assertNotNull(coord1);
+		assertNotNull(coord2);
+		assertNotNull(coord3);
+		assertEquals(coord1.getTime(), 0);
+		assertEquals(coord2.getTime(), 50);
+		assertEquals(coord3.getTime(), 100);
+	}
+	
+	@Test
+	public void compareToTest() {
+		assertEquals(coord1.compareTo(coord2), -1);
+		assertEquals(coord1.compareTo(coord3), -1);
+		assertEquals(coord1.compareTo(coord4), 0);
+		assertEquals(coord2.compareTo(coord1), 1);
+		assertEquals(coord2.compareTo(coord3), -1);
+	}
+}
