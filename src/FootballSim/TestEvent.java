fd4fb3579f4831e1c058c541eb0046433ed5c8bd
Rick Wight <fmwight@wpi.edu>
2015-11-09 12:43:13 -0500

    Added copy constructor to TestEvent class

--- src/FootballSim/TestEvent.java
+++ src/FootballSim/TestEvent.java
@@ -5,6 +5,10 @@ public class TestEvent extends absFootballEvent implements IFootballEvent {
 	public TestEvent(int time) {
 		super(time);
 	}
+	
+	public TestEvent(IFootballEvent otherEvent) {
+		super(otherEvent.getTime());
+	}
 
 	@Override
 	public void executeEvent(FootballGame theGame) throws FootballException {
